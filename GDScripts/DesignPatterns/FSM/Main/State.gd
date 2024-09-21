ecba69851bbb88a8fbbaef9f52747e674743251e
Cody <codrin.ilie10@gmail.com>
2024-09-21 11:40:06 +0200

    State machine first implementation
     - Added the base code for the state machine
     - Added the base class for the State
     - Added a new idle_state for testing purposes

---
+++ GDScripts/DesignPatterns/FSM/Main/State.gd
@@ -0,0 +1,16 @@
+extends Node3D
+class_name State
+
+signal state_transition
+
+func Enter() -> void:
+	pass
+
+func Exit() -> void:
+	pass
+
+func Update(_delta:float) -> void:
+	pass
+
+func Physics_Update(_delta:float) -> void:
+	pass
---
+++ GDScripts/DesignPatterns/FSM/States/idle_state.gd
@@ -0,0 +1,9 @@
+extends State
+class_name PlayerIdle
+
+func Enter():
+	pass
+func Exit():
+	pass
+func Update(_delta):
+	pass
