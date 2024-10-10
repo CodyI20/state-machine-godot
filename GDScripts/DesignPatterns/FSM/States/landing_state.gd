4b58450feb3fe91ac1d5f7fa3b67c2b95fcd252e
Cody <codrin.ilie10@gmail.com>
2024-10-10 17:54:37 +0200

    Minor fixes
     - Added the transition from the landing state to the jumping state
     - Changed the jump animation framerate

--- GDScripts/DesignPatterns/FSM/States/landing_state.gd
+++ GDScripts/DesignPatterns/FSM/States/landing_state.gd
@@ -18,8 +18,10 @@ func Physics_Update(_delta:float) -> void:
 	pass
 	
 func StateSwitchLogic() -> void:
-	if (Input.is_action_pressed("Left") or Input.is_action_pressed("Right")) and player.is_on_floor():
+	if (Input.is_action_pressed("Left") or Input.is_action_pressed("Right")):
 		state_transition.emit(self, "Walking")
+	if Input.is_action_just_pressed("Jump"):
+		state_transition.emit(self, "Jumping")
 
 # Signal from the AnimatedSprite2D so the animation can fully play before entering Idle state
 func AnimationFinished() -> void:
